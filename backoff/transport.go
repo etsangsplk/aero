76d50baa84ec0c5ea8a982c748091457a5a4175d
Matthew Conger-Eldeen <mceldeen@gmail.com>
2014-01-04 15:22:25 -0700

    Fix formatting

--- backoff/transport.go
+++ backoff/transport.go
@@ -20,17 +20,13 @@ func NewHttpTransport(b Backoffer) *HttpTransport {
 
 func (t *HttpTransport) RoundTrip(req *http.Request) (res *http.Response, err error) {
     n := uint(0)
-
     for delay, next := t.b.Next(n); next; delay, next = t.b.Next(n) {
         res, err = t.Transport.RoundTrip(req)
-
         if err == nil {
             return
         }
-
         <-time.After(delay)
         n++
     }
-
     return
 }
