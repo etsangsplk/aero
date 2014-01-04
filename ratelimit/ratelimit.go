7fda73f7908d635ca0d3e1c51fb993133e6d6c6c
Matthew Conger-Eldeen <mceldeen@gmail.com>
2014-01-04 13:18:59 -0700

    Add bursty rate limiter

---
+++ ratelimit/ratelimit.go
@@ -0,0 +1,61 @@
+package ratelimit
+
+import (
+    "time"
+)
+
+type RateLimiter interface {
+    Tick() <-chan time.Time
+    Stop()
+}
+
+type Bursty struct {
+    c   chan time.Time
+    n   int
+    t   time.Duration
+    cls chan bool
+}
+
+func NewBursty(num int, unit time.Duration, burst int) *Bursty {
+    l := &Bursty{
+        c:   make(chan time.Time, burst),
+        cls: make(chan bool, 1),
+        n:   num,
+        t:   unit,
+    }
+
+    l.start(burst)
+
+    return l
+}
+
+func (l *Bursty) Tick() <-chan time.Time {
+    return l.c
+}
+
+func (l *Bursty) Stop() {
+    l.cls <- true
+}
+
+func (l *Bursty) start(burst int) {
+    for i := 0; i < burst; i++ {
+        l.c <- time.Now()
+    }
+
+    go func() {
+        delta := l.t / time.Duration(l.n)
+        ticker := time.NewTicker(delta)
+        defer ticker.Stop()
+
+        for {
+            select {
+            case now := <-ticker.C:
+                go func() {
+                    l.c <- now
+                }()
+            case <-l.cls:
+                return
+            }
+        }
+    }()
+}
---
+++ ratelimit/ratelimit_test.go
@@ -0,0 +1,37 @@
+package ratelimit
+
+import (
+    "testing"
+    "time"
+)
+
+func TestBurstyRateLimit(t *testing.T) {
+    const count = 4
+    delta := 10 * time.Millisecond
+    burst := 3
+
+    limiter := NewBursty(1, delta, burst)
+
+    t0 := time.Now()
+    for i := 0; i < count; i++ {
+        <-limiter.Tick()
+    }
+    t1 := time.Now()
+    dt := t1.Sub(t0)
+
+    limiter.Stop()
+
+    target := delta * time.Duration(count-burst)
+    slop := target * 2 / 10
+    if dt < target-slop || (!testing.Short() && dt > target+slop) {
+        t.Fatalf("%d %s ticks with a burst of %d took %s, expected [%s,%s]", count, delta, burst, dt, target-slop, target+slop)
+    }
+
+    // Now test that the ticker stopped
+    select {
+    case <-limiter.Tick():
+        t.Fatal("Ticker did not shut down")
+    default:
+        // ok
+    }
+}
